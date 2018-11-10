373d2bedc014e5403b76f786f1074e7bfc976a8d
Michael J. Simons <michael@simons.ac>
2018-11-10 14:01:19 +0100

    Fix script.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
 (cd ../knowledge && ./mvnw clean verify)
-cp ../knowledge/target/knowledge-0.0.1-SNAPSHOT.jar -d knowledge/knowledge.jar
+cp ../knowledge/target/knowledge-0.0.1-SNAPSHOT.jar knowledge/knowledge.jar
 
 docker-compose build
