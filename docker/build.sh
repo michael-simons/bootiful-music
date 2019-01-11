a94e63329e1474ab3ab9f0e9c4953a36ab12081a
Michael Simons <michael@simons.ac>
2019-01-11 12:08:59 +0100

    Upgrade to Neo4j 3.5.1.

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eu
 
+(cd ../etl && ./mvnw clean verify)
+cp ../etl/target/etl-0.0.1-SNAPSHOT.jar var/knowledgedb/plugins/
+
 (cd ../knowledge && ./mvnw clean verify)
 cp ../knowledge/target/knowledge-0.0.1-SNAPSHOT.jar knowledge/knowledge.jar
 
